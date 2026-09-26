67bb787c55a95fb06b497505db0db0fdb97fb2d9
jsonicdev <serikabdez05@gmail.com>
2026-09-26 17:04:08 +0500

    feat: slider done

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const themeSwitch = document.querySelector('.theme__switch')
+const links = document.querySelectorAll('.navbar__container a')
+
+const burger = document.getElementById('burger')
+const menu = document.getElementById('menu')
+
+const slides = document.querySelectorAll('.slide')
+const dots = document.querySelectorAll('.dot')
+
+const prev = document.querySelector('.prev')
+const next = document.querySelector('.next')
+
+let currentSlider = 0
 
 themeSwitch.addEventListener('click', () => {
 	const isDark = document.body.classList.toggle('dark')
@@ -6,9 +18,65 @@ themeSwitch.addEventListener('click', () => {
 	localStorage.setItem('theme', isDark ? 'dark' : 'light')
 })
 
-const burger = document.getElementById('burger')
-const menu = document.getElementById('menu')
-
 burger.addEventListener('click', () => {
 	menu.classList.toggle('active')
+	document.body.classList.toggle('no-scroll')
+})
+
+function closeMenu() {
+	menu.classList.remove('active')
+	document.body.classList.remove('no-scroll')
+}
+
+links.forEach(link => {
+	link.addEventListener('click', () => {
+		closeMenu()
+	})
+})
+
+document.addEventListener('keydown', event => {
+	if (event.key === 'Escape') {
+		closeMenu()
+	}
+})
+
+function showSlide(index) {
+	slides.forEach(slide => {
+		slide.classList.remove('active')
+	})
+
+	dots.forEach(dot => {
+		dot.classList.remove('active')
+	})
+
+	slides[index].classList.add('active')
+	dots[index].classList.add('active')
+
+	currentSlider = index
+}
+
+next.addEventListener('click', () => {
+	let nextSlide = currentSlider + 1
+
+	if (nextSlide >= slides.length) {
+		nextSlide = 0
+	}
+
+	showSlide(nextSlide)
+})
+
+prev.addEventListener('click', () => {
+	let prevSlide = currentSlider - 1
+
+	if (prevSlide < 0) {
+		prevSlide = slides.length - 1
+	}
+
+	showSlide(prevSlide)
+})
+
+dots.forEach((dot, index) => {
+	dot.addEventListener('click', () => {
+		showSlide(index)
+	})
 })
